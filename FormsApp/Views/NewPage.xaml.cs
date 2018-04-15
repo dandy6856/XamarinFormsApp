f438f52aee411d2e3d522b089ad7329b63571a08
dandy6856 <dandy6856@gmail.com>
2018-04-15 09:59:09 +0700

    New file testing

---
+++ FormsApp/Views/NewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FormsApp.Views
+{
+    public partial class NewPage : ContentPage
+    {
+        public NewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
